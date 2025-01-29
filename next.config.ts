24aaeae549f4260cfb5f3d5307acdb521cd68755
pranoymajumdar <189314177+pranoymajumdar@users.noreply.github.com>
2025-01-30 00:52:12 +0530

    removed cf pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
